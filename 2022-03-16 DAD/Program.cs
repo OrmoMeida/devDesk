7e6ad941aa8bd4ba820ce77bdae0e1ad3b3cf020
IsabelleOrmo <bellecrenonini@gmail.com>
2022-03-16 15:32:08 -0300

    Adicionar arquivos de projeto.

---
+++ 2022-03-16 DAD/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _2022_03_16_DAD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Projeto1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Projeto1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
