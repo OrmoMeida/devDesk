c79a7b4c06e0372860e6e133fab17a8d927e7147
IsabelleOrmo <bellecrenonini@gmail.com>
2022-03-29 15:10:06 -0300

    Exercício 4

--- listaProgramacaoEstruturada/listaProgramacaoEstruturada/Program.cs
+++ listaProgramacaoEstruturada/listaProgramacaoEstruturada/Program.cs
@@ -7,25 +7,9 @@ namespace listaProgramacaoEstruturada
         static void Main(string[] args)
         {
             Exercicio exercicios = new Exercicio(); 
-            
-            Console.WriteLine("Exercício 1:  ");
-            exercicios.Ex1();
-
-            Console.WriteLine("Exercício 2:  ");
-            exercicios.Ex2();
-
-            Console.WriteLine("Exercício 3:  ");
-            exercicios.Ex3();
 
             Console.WriteLine("Exercício 4:  ");
             exercicios.Ex4();
-
-            Console.WriteLine("Exercício 5:  ");
-            exercicios.Ex5();
-
-            Console.WriteLine("Exercício 6:  ");
-            exercicios.Ex6();
-
         }
     }
 }
