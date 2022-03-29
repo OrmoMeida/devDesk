fd298124fb5f5920b2800745ee1519ee748648d0
IsabelleOrmo <bellecrenonini@gmail.com>
2022-03-29 15:07:58 -0300

    Exercício 2

--- listaProgramacaoEstruturada/listaProgramacaoEstruturada/Program.cs
+++ listaProgramacaoEstruturada/listaProgramacaoEstruturada/Program.cs
@@ -8,24 +8,8 @@ namespace listaProgramacaoEstruturada
         {
             Exercicio exercicios = new Exercicio(); 
             
-            Console.WriteLine("Exercício 1:  ");
-            exercicios.Ex1();
-
             Console.WriteLine("Exercício 2:  ");
             exercicios.Ex2();
-
-            Console.WriteLine("Exercício 3:  ");
-            exercicios.Ex3();
-
-            Console.WriteLine("Exercício 4:  ");
-            exercicios.Ex4();
-
-            Console.WriteLine("Exercício 5:  ");
-            exercicios.Ex5();
-
-            Console.WriteLine("Exercício 6:  ");
-            exercicios.Ex6();
-
         }
     }
 }
