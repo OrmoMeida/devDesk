debe71bc4bb9853d1198e33a188f8b3a16649b6f
IsabelleOrmo <bellecrenonini@gmail.com>
2022-03-29 13:56:03 -0300

    Main arrumada

--- listaProgramacaoEstruturada/listaProgramacaoEstruturada/Program.cs
+++ listaProgramacaoEstruturada/listaProgramacaoEstruturada/Program.cs
@@ -10,6 +10,22 @@ namespace listaProgramacaoEstruturada
             
             Console.WriteLine("Exercício 1:  ");
             exercicios.Ex1();
+
+            Console.WriteLine("Exercício 2:  ");
+            exercicios.Ex2();
+
+            Console.WriteLine("Exercício 3:  ");
+            exercicios.Ex3();
+
+            Console.WriteLine("Exercício 4:  ");
+            exercicios.Ex4();
+
+            Console.WriteLine("Exercício 5:  ");
+            exercicios.Ex5();
+
+            Console.WriteLine("Exercício 6:  ");
+            exercicios.Ex6();
+
         }
     }
 }
