0bacedd0372eb6d7197f4f2544d57be02aa05f7f
IsabelleOrmo <bellecrenonini@gmail.com>
2022-03-29 13:53:47 -0300

    Branch do Ex1

--- listaProgramacaoEstruturada/listaProgramacaoEstruturada/Program.cs
+++ listaProgramacaoEstruturada/listaProgramacaoEstruturada/Program.cs
@@ -10,22 +10,6 @@ namespace listaProgramacaoEstruturada
             
             Console.WriteLine("Exercício 1:  ");
             exercicios.Ex1();
-
-            Console.WriteLine("Exercício 2:  ");
-            exercicios.Ex2();
-
-            Console.WriteLine("Exercício 3:  ");
-            exercicios.Ex3();
-
-            Console.WriteLine("Exercício 4:  ");
-            exercicios.Ex4();
-
-            Console.WriteLine("Exercício 5:  ");
-            exercicios.Ex5();
-
-            Console.WriteLine("Exercício 6:  ");
-            exercicios.Ex6();
-
         }
     }
 }
